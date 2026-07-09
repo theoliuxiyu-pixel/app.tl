daec3b74d1195ad29f4fa46363f9afaffe5598b6
theoliuxiyu-pixel <theoliuxiyu@gmail.com>
2026-07-09 22:03:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if not st.session_state.authenticated:
     st.title("🔒 咪姐秘密基地")
     password = st.text_input("輸入密碼", type="password")
     if st.button("解鎖"):
-        if password == "Meow123":
+        if password == "71398426":
             st.session_state.authenticated = True
             st.session_state.last_login_date = get_tw_date()
             st.rerun()
