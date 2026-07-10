4df6d209be42ba0a364cc67ec226cd6e9fec2623
theoliuxiyu-pixel <theoliuxiyu@gmail.com>
2026-07-10 11:44:07 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
-# database.py
-from supabase import create_client
 import streamlit as st
+from supabase import create_client
 from datetime import datetime, timezone, timedelta
 
-# 建立資料庫連線物件
 supabase = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
 
 def add_to_diary(message):
@@ -13,9 +11,7 @@ def add_to_diary(message):
             "message": message
         }).execute()
         return True
-    except Exception as e:
-        st.error(f"資料庫寫入失敗: {e}")
-        return False
+    except: return False
 
 def get_recent_logs(limit=10):
     return supabase.table("diary").select("*").order("id", desc=True).limit(limit).execute().data
